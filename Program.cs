d5b1d67b161a71abd7a4045202a32ac21427d502
Hidayathullah DS <55664140+Hidayath87@users.noreply.github.com>
2022-11-20 22:06:42 +0530

    Add files via upload
    Converting to hexadecimal

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        // hexadecimal number as string
+        string input = "56304";
+        int output = 0;
+
+        // converting to integer
+        output = Convert.ToInt32(input, 16);
+
+        // to print  the value
+        Console.WriteLine("Integer number: " + output);
+    }
+}
