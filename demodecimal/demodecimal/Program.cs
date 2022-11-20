44dd3726b679f195ef3b0c5c31611b5a6b79e907
Hidayathullah DS <55664140+Hidayath87@users.noreply.github.com>
2022-11-20 21:44:34 +0530

    Add files via upload
    Converting to decimal

---
+++ demodecimal/demodecimal/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+public class Demo
+{
+    public static void Main()
+    {
+        decimal decimalVal;
+        string stringVal = "2,345.26";
+        decimalVal = System.Convert.ToDecimal(stringVal);
+        System.Console.WriteLine("String converted to decimal = {0} ", decimalVal);
+    }
+}
