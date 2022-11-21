8a38fad6c97fb1613ca55276738f903c7f57086a
Hidayathullah DS <55664140+Hidayath87@users.noreply.github.com>
2022-11-21 09:30:07 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+class GFG
+{
+
+    static void Main(string[] args)
+    {
+        // binary number as string
+        string bin_strng = "1010101010101010";
+        int number = 0;
+
+        // converting to integer
+        number = Convert.ToInt32(bin_strng, 2);
+
+        // to print  the value
+        Console.WriteLine("Number value of binary \"{0}\" is = {1}", bin_strng,
+                          number);
+    }
+}
